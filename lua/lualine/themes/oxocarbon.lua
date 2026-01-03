20cf372b89171ba3b7608d88bb38eb6bddd1e53d
Rin Brk <dareka826@gmail.com>
2026-01-03 01:43:24 +0100

    Restructure and adjust the lualine theme

--- lua/lualine/themes/oxocarbon.lua
+++ lua/lualine/themes/oxocarbon.lua
@@ -1,48 +1,63 @@
 local oxocarbon = require("oxocarbon").oxocarbon
 
 local colors = {
-  line_bg = oxocarbon.base00,
+  line_bg = oxocarbon.base01,
   line_fg = oxocarbon.base04,
 
-  normal_bg  = oxocarbon.base09,
-  insert_bg  = oxocarbon.base12,
-  visual_bg  = oxocarbon.base14,
-  replace_bg = oxocarbon.base10,
-  command_bg = oxocarbon.base13,
-  mode_fg    = oxocarbon.base02,
-
-  replace_alt_fg = oxocarbon.base08,
+  normal_bg   = oxocarbon.base09,
+  insert_bg   = oxocarbon.base12,
+  visual_bg   = oxocarbon.base14,
+  replace_bg  = oxocarbon.base10,
+  command_bg  = oxocarbon.base13,
+  terminal_bg = oxocarbon.base07,
+  mode_fg     = oxocarbon.base02,
 }
 
+local default = {fg = colors.line_fg, bg = colors.line_bg}
+
+local normal_mode   = {fg = colors.mode_fg, bg = colors.normal_bg}
+local insert_mode   = {fg = colors.mode_fg, bg = colors.insert_bg}
+local visual_mode   = {fg = colors.mode_fg, bg = colors.visual_bg}
+local replace_mode  = {fg = colors.mode_fg, bg = colors.replace_bg}
+local command_mode  = {fg = colors.mode_fg, bg = colors.command_bg}
+local terminal_mode = {fg = colors.mode_fg, bg = colors.terminal_bg}
+
+-- [ A | B | C        X | Y | Z ]
 local theme = {
   normal = {
-    a = {fg = colors.mode_fg, bg = colors.normal_bg},
-    b = {fg = colors.line_fg, bg = colors.line_bg},
-    c = {fg = colors.line_fg, bg = colors.line_bg},
-    z = {fg = colors.line_fg, bg = colors.line_bg},
+    a = normal_mode, z = normal_mode,
+    b = default, y = default,
+    c = default, x = default,
   },
   insert = {
-    a = {fg = colors.mode_fg, bg = colors.insert_bg},
-    b = {fg = colors.line_fg, bg = colors.line_bg},
-    z = {fg = colors.insert_bg, bg = colors.line_bg},
+    a = insert_mode, z = insert_mode,
+    b = default, y = default,
+    c = default, x = default,
   },
   visual = {
-    a = {fg = colors.mode_fg, bg = colors.visual_bg},
-    b = {fg = colors.line_fg, bg = colors.line_bg},
-    y = {fg = colors.line_fg, bg = colors.line_bg},
-    z = {fg = colors.insert_bg, bg = colors.line_bg},
+    a = visual_mode, z = visual_mode,
+    b = default, y = default,
+    c = default, x = default,
   },
   replace = {
-    a = {fg = colors.mode_fg, bg = colors.replace_bg},
-    b = {fg = colors.replace_alt_fg, bg = colors.line_bg},
-  },
-  inactive = {
-    a = {fg = colors.mode_fg, bg = colors.normal_bg},
-    b = {fg = colors.line_fg, bg = colors.line_bg},
-    z = {fg = colors.mode_fg, bg = colors.line_bg},
+    a = replace_mode, z = replace_mode,
+    b = default, y = default,
+    c = default, x = default,
   },
   command = {
-    a = {fg = colors.mode_fg, bg = colors.command_bg},
+    a = command_mode, z = command_mode,
+    b = default, y = default,
+    c = default, x = default,
+  },
+  terminal = {
+    a = terminal_mode, z = terminal_mode,
+    b = default, y = default,
+    c = default, x = default,
+  },
+  inactive = {
+    a = default, z = default,
+    b = default, y = default,
+    c = default, x = default,
   },
 }
 
